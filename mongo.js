b65502174692f01b42a156e4c12abe1ba963b138
Sam Vitare <sandhya.palanki@gmail.com>
2023-10-12 13:57:40 -0500

    practice part 3c - saving data to mongodb, creating and saving objects

--- mongo.js
+++ mongo.js
@@ -9,8 +9,11 @@ const password = process.argv[2];
 const uri = `mongodb+srv://sam:${password}@cluster0.gkzzbka.mongodb.net/noteApp?retryWrites=true&w=majority`;
 
 mongoose.set("strictQuery", false);
+
+// connect to the mongodb database
 mongoose.connect(uri);
 
+// define a schema for the note
 const noteSchema = new mongoose.Schema({
   content: String,
   important: Boolean,
@@ -19,12 +22,16 @@ const noteSchema = new mongoose.Schema({
 // the first "Note" parameter is the singular name of the model
 const Note = mongoose.model("Note", noteSchema);
 
+// create a new note object with the help of the "Note" model
 const note = new Note({
   content: "HTML is Easy",
   important: true,
 });
 
+// save the object to the database
 note.save().then((result) => {
   console.log("note saved!");
+  console.log(result);
+  // close the database connection
   mongoose.connection.close();
 });
