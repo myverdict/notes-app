bfbdcb3d0e48f1ce7b18e48576db18c9d139a895
Sam Vitare <sandhya.palanki@gmail.com>
2023-10-12 16:19:23 -0500

    practice part 3c - saving data to mongodb, using database in route handlers

--- index.js
+++ index.js
@@ -81,6 +81,23 @@ app.get("/api/notes", (request, response) => {
 //   response.json(note);
 // });
 
+app.post("/api/notes", (request, response) => {
+  const body = request.body;
+
+  if (body.content === undefined) {
+    return response.status(400).json({ error: "content missing" });
+  }
+
+  const note = new Note({
+    content: body.content,
+    important: body.important || false,
+  });
+
+  note.save().then((savedNote) => {
+    response.json(savedNote);
+  });
+});
+
 // app.get("/api/notes/:id", (request, response) => {
 //   const id = Number(request.params.id);
 //   const note = notes.find((note) => note.id === id);
@@ -93,6 +110,12 @@ app.get("/api/notes", (request, response) => {
 
 //   response.json(note);
 // });
+
+app.get("/api/notes/:id", (request, response) => {
+  Note.findById(request.params.id).then((note) => {
+    response.json(note);
+  });
+});
 
 // app.delete("/api/notes/:id", (request, response) => {
 //   const id = Number(request.params.id);
