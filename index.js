c786bb729150b5ec558e555da2ccc18a2eae6758
Sam Vitare <sandhya.palanki@gmail.com>
2023-10-13 09:35:59 -0500

    practice part 3c - saving data to mongodb; other operations

--- index.js
+++ index.js
@@ -59,12 +59,37 @@ app.post("/api/notes", (request, response) => {
   });
 });
 
+app.put("/api/notes/:id", (request, response, next) => {
+  const body = request.body;
+
+  const note = {
+    content: body.content,
+    important: body.important,
+  };
+
+  Note.findByIdAndUpdate(request.params.id, note, { new: true })
+    .then((updatedNote) => {
+      console.log("updated note");
+      console.log(updatedNote);
+      response.json(updatedNote);
+    })
+    .catch((error) => next(error));
+});
+
 // app.delete("/api/notes/:id", (request, response) => {
 //   const id = Number(request.params.id);
 //   notes = notes.filter((note) => note.id !== id);
 
 //   response.status(204).end();
 // });
+
+app.delete("/api/notes/:id", (request, response, next) => {
+  Note.findByIdAndRemove(request.params.id)
+    .then((result) => {
+      response.status(204).end();
+    })
+    .catch((error) => next(error));
+});
 
 const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: "unknown endpoint" });
