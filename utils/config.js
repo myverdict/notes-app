957050e9eb6085dc59addb1adaa6eeeb40daa971
unknown <26878263+myverdict@users.noreply.github.com>
2024-07-30 13:38:28 -0400

    Part 5d - End to end testing with PW; added console logs in the config

--- utils/config.js
+++ utils/config.js
@@ -1,4 +1,5 @@
 // handling of environment variables is extracted into a separate "utils/config.js" file
+
 require('dotenv').config();
 
 const PORT = process.env.PORT;
@@ -6,6 +7,10 @@ const PORT = process.env.PORT;
 const MONGODB_URI = process.env.NODE_ENV === 'test'
   ? process.env.TEST_MONGODB_URI
   : process.env.MONGODB_URI;
+
+console.log();
+console.log('Node process version: ', process.version);
+console.log('Environment: ', process.env.NODE_ENV);
 
 module.exports = {
   MONGODB_URI,
